b4e517b03d3326f16a214ceb545aea7b267c8fb8
李国颖 <liguoying@iiotos.com>
2019-09-05 17:11:58 +0800

    single_number

---
+++ 136_single_number.py
@@ -0,0 +1,58 @@
+'''
+@Description: 
+@Version: 
+@Author: liguoying
+@Date: 2019-09-05 16:49:49
+'''
+
+from typing import List
+
+### 题目描述
+"""
+给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+说明：
+
+你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+示例 1:
+----
+输入: [2,2,1]
+输出: 1
+
+示例 2:
+----
+输入: [4,1,2,1,2]
+输出: 4
+"""
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # Method 1
+        # dics = {}
+        # for i in nums:
+        #     if i not in dics.keys():
+        #         dics[i] = 1
+        #     else:
+        #         dics[i] += 1
+        
+        # rev_dict = {v: k for k,v in dics.items()}
+        # return rev_dict[1]
+
+        # Method 2
+        # unique_list = list(set(nums))
+        # sum1 = sum(unique_list) * 2
+        # sum2 = sum(nums)
+        # return sum1 - sum2
+
+        # Method 3
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
+
+if __name__ == "__main__":
+    nums = [4,1,2,1,2]
+    # nums = [2,2,1]
+    s = Solution()
+    print(s.singleNumber(nums))
---
+++ 2_add_two_numbers.py
@@ -0,0 +1,55 @@
+'''
+@Description: 
+@Version: 
+@Author: liguoying
+@Date: 2019-09-05 16:06:22
+'''
+from typing import List
+
+### 题目描述
+"""
+给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例
+-----
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+"""
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        ###
+        # wrong answer
+        ###
+        list1 = []
+        list2 = []
+        while l1:
+            list1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            list2.append(l2.val)
+            l2 = l2.next
+        
+        len1 = len(list1)
+        len2 = len(list2)
+        num1 = int("".join(list1[::-1]))
+        num2 = int("".join(list2[::-1]))
+        add_num = num1 + num2
+        num_str = str(add_num)
+        rev_num_str = num_str[::-1]
+        return [int(n) for n in rev_num_str.split()]
+
+
+if __name__ == "__main__":
+    l1 = []
